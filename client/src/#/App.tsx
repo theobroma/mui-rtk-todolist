8a4962fb2d1db918950a0c0afc1bca00d936ff8b
Alexandr <theobroma222@gmail.com>
2020-08-19 02:02:49 +0300

    add MUI

--- client/src/#/App.tsx
+++ client/src/#/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import Layout from '../@components/Layout';
 
 interface Props {}
 
 export const App = (props: Props) => {
   return (
-    <div>
-      <span>App</span>
-    </div>
+    <Layout>
+      {/* <AddTodo
+        inputValue={inputValue}
+        onInputChange={changeInput}
+        onButtonClick={clearInputAndAddTodo}
+        onInputKeyPress={event => keyInput(event, clearInputAndAddTodo)}
+      />
+      <TodoList
+        items={todos}
+        onItemCheck={idx => checkTodo(idx)}
+        onItemRemove={idx => removeTodo(idx)}
+      /> */}
+      layout
+    </Layout>
   );
 };
---
+++ client/src/@components/Layout.tsx
@@ -0,0 +1,19 @@
+import React, { memo } from 'react';
+import { AppBar, Toolbar, Typography, Paper } from '@material-ui/core';
+
+const Layout = memo((props) => (
+  <Paper
+    elevation={0}
+    style={{ padding: 0, margin: 0, backgroundColor: '#f3f3f3' }}
+  >
+    <AppBar color="primary" position="static" style={{ height: 64 }}>
+      <Toolbar style={{ height: 64 }}>
+        <Typography color="inherit">TODO APP</Typography>
+      </Toolbar>
+    </AppBar>
+
+    {props.children}
+  </Paper>
+));
+
+export default Layout;
